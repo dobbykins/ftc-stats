b48e86c477defdc703866cc0d1ba08ada6cd7798
dobbykins <dantgcm21@gmail.com>
2026-05-08 17:30:38 -0400

    Changing the piecewise function for the m factor.

--- src/utils/epa.js
+++ src/utils/epa.js
@@ -40,7 +40,7 @@ export const toU = (epa, avg) => avg ? epa / avg : 0;
 export function mFactor(n)
 {
   if (n <= 10)         return 0;
-  else if (n <= 15)    return (1/24)*(n-10);
+  else if (n <= 15)    return (1/5)*(n-10);
   else                 return 1;
 }
 export function kFactor(n) {
