aef08e1ac06fa539ce232320b8c982b6801f91e4
dobbykins <dantgcm21@gmail.com>
2026-05-08 21:14:48 -0400

    More changes to the M factor

--- src/utils/epa.js
+++ src/utils/epa.js
@@ -39,9 +39,9 @@ export const toU = (epa, avg) => avg ? epa / avg : 0;
 
 export function mFactor(n)
 {
-  if (n <= 10)         return 1;
-  else if (n <= 17)    return (1)/((n+1)-10);
-  else                 return 0.25;
+  if (n <= 10)         return 0.25;
+  else if (n <= 17)    return (1/14)*(n-10);
+  else                 return 0.5;
 }
 export function kFactor(n) {
   if (n <= K_RAMP_START)  return K_FLOOR;
