55f9d272cf3b68fcb7607baa1ae66f5b55421287
dobbykins <dantgcm21@gmail.com>
2026-05-09 01:05:20 -0400

    asdfg

--- src/utils/epa.js
+++ src/utils/epa.js
@@ -5,8 +5,8 @@
 // K-factor shape
 const K_RAMP_START   = 2;
 const K_RAMP_END     = 10;
-const K_PEAK_VAL     = 0.25;
-const K_FLOOR        = 0.65;
+const K_PEAK_VAL     = 0.3;
+const K_FLOOR        = 0.5;
 
 const AUTO_PRIOR_FRAC    = 0.15;
 const PATTERN_PRIOR_FRAC = 0.0;
