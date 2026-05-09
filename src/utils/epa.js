312abde576685c48d33d4ba2755e6815ffad4a0f
dobbykins <dantgcm21@gmail.com>
2026-05-08 21:11:17 -0400

    Increased the K factor

--- src/utils/epa.js
+++ src/utils/epa.js
@@ -5,8 +5,8 @@
 // K-factor shape
 const K_RAMP_START   = 2;
 const K_RAMP_END     = 5;
-const K_PEAK_VAL     = 0.5;
-const K_FLOOR        = 0.7;
+const K_PEAK_VAL     = 0.75;
+const K_FLOOR        = 0.95;
 
 const AUTO_PRIOR_FRAC    = 0.20;
 const PATTERN_PRIOR_FRAC = 0.0;
